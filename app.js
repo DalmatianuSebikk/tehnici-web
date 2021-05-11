6c8e6454c3e76a94b525015d0bbc0271610a1a04
DalmatianuSebikk <se3bofficial@gmail.com>
2021-05-11 13:37:07 +0300

    solved issues with MIME CSS and added the ip getter

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+
 // var engine = require('ejs-mate');
 const app = express(); // declar obiectul
 // Setez documentul default de views ca fiind drept folderul views
 app.set('views', path.join(__dirname, 'views')); // folosesc app.set pentru asta
 
 // setez folderele pentru SASS si js
-app.set(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "assets")));
+app.use(express.static(path.join(__dirname, "css")));
 app.set(express.static(path.join(__dirname, 'node_modules')));
 app.set(express.static(path.join(__dirname, 'js')));
+
+// fac rutele, ip ul si o functie de aruncat 404 not found:
+
+app.use('/', routes);
+app.use(function(req, res) {
+    res.status(404);
+    res.render('pagini/eroare');
+});
 
 // setez view-engine ul la ejs
 app.set('view engine', 'ejs');
 // app.engine('ejs', engine);
-// fac rutele
-app.use('/', routes);
 
 app.listen(8080, () => {console.log("Serverul a pornit...")});
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const requestIp = require('request-ip');
+
+const ipMiddleware = function(req, res, next) {
+  const clientIp = requestIp.getClientIp(req); 
+  next();
+};
+
+router.use(requestIp.mw());
+// router.use(function(req, res){
+  // const ip = req.clientIp;
+//   res.end(ip);
+// });
 
 router.get('/', (req, res) => {
+  const ip = req.clientIp;
   console.log('Request pentru pagina principala. Returnez index.ejs');
-  res.render('index.ejs');
+  res.render('pagini/index.ejs', {ip: ip});
 });
 
+router.get('/index', (req, res) => {
+  console.log('Request pentru pagina principala. Returnez index.ejs');
+  res.render('pagini/index.ejs');
+});
+
+router.get('/investigatii', (req, res) => {
+  console.log('Request pentru pagina de investigatii. Returnez investigatii.ejs');
+  res.render('pagini/investigatii.ejs');
+});
+
+
+router.get('/departamente', (req, res) => {
+  console.log('Request pentru pagina de departamente. Returnez departamente.ejs');
+  res.render('pagini/departamente.ejs');
+})
 
 module.exports = router;
