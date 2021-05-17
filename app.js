7ef11d40373736cbd2b8142f449ef7032a54b322
DalmatianuSebikk <se3bofficial@gmail.com>
2021-05-17 22:42:52 +0300

    Last modifications

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const routes = require('./routes');
-
+const sharp = require('sharp');
+const { S_IFREG } = require('constants');
 // var engine = require('ejs-mate');
 const app = express(); // declar obiectul
 // Setez documentul default de views ca fiind drept folderul views
 app.set('views', path.join(__dirname, 'views')); // folosesc app.set pentru asta
 
 // setez folderele pentru SASS si js
-app.use(express.static(path.join(__dirname, "assets")));
+
+// app.use(express.static(path.join(__dirname, "assets/galerie")));
 app.use(express.static(path.join(__dirname, "css")));
 app.set(express.static(path.join(__dirname, 'node_modules')));
 app.set(express.static(path.join(__dirname, 'js')));
 
 // fac rutele, ip ul si o functie de aruncat 404 not found:
 
+app.use('/assets/galerie.json', (req, res) =>{
+    res.status(403);
+    res.render('pagini/interzis');
+  });
+
+app.use('/assets', express.static(path.join(__dirname, "assets")));
 app.use('/', routes);
-app.use(function(req, res) {
+app.use('/*',function(req, res) {
     res.status(404);
     res.render('pagini/eroare');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,69 @@
 const express = require('express');
 const router = express.Router();
 const requestIp = require('request-ip');
-
+const fs = require('fs');
+const sharp = require('sharp');
+const path = require('path');
 const ipMiddleware = function(req, res, next) {
   const clientIp = requestIp.getClientIp(req); 
   next();
 };
+
+function alegRandom(){
+  arrayNumarPoze = [9, 12];
+  var nrRandom = Math.floor((Math.random() * 10) % 2); // dintre 0 si 1
+  return arrayNumarPoze[nrRandom];
+}
+
+function alegImagini() {
+  var numarPoze = alegRandom();
+  var textFisier = fs.readFileSync('assets/galerie.json');
+  var jsonParsat = JSON.parse(textFisier);
+  var caleGalerie = 'assets/' + jsonParsat.cale_galerie;
+  let arrayImagini = [];
+
+  for(let imagini of jsonParsat.imagini){
+    let imagine = path.posix.join(caleGalerie, imagini.cale_fisier);
+    let extensie = path.extname(imagini.cale_fisier);
+    let numeFisier = path.basename(imagini.cale_fisier, extensie);
+    
+    arrayImagini.push({mare: imagine, descriere: imagini.text_descriere});
+  }
+
+  return arrayImagini;
+}
+
+function verificareImagini(){
+  var data = new Date();
+  var numar_luna = data.getMonth();
+  var textFisier = fs.readFileSync('assets/galerie.json');
+  var jsonParsat = JSON.parse(textFisier); // l-am transformat in obiect;
+
+  var caleGalerie = 'assets/' + jsonParsat.cale_galerie;
+  let arrayImagini = [];
+
+  for(let imagini of jsonParsat.imagini){
+      let imagine = path.posix.join(caleGalerie, imagini.cale_fisier);
+      console.log(imagine);
+      let extensie = path.extname(imagini.cale_fisier);
+      let numeFisier = path.basename(imagini.cale_fisier,extensie);
+      let imagineNoua = path.posix.join(caleGalerie + '/mic/' + numeFisier + '-mic' + '.jpg'); // creez imaginea noua
+
+      if(!fs.existsSync(imagineNoua)){
+        sharp(imagine).resize(150).toFile(imagineNoua, function(err){
+            if(err){
+                console.log("eroare de conversie:", imagine, "->", imagineNoua);
+            }
+        });
+    }
+
+      if(imagini.luni.indexOf(numar_luna) >= 0){
+          arrayImagini.push({mare: imagine, mic: imagineNoua, descriere: imagini.text_descriere});
+      } 
+  }
+  console.log(arrayImagini);
+  return arrayImagini;
+}
 
 router.use(requestIp.mw());
 // router.use(function(req, res){
@@ -13,16 +71,25 @@ router.use(requestIp.mw());
 //   res.end(ip);
 // });
 
-router.get('/', (req, res) => {
+
+
+router.get(['/', '/index'], (req, res) => {
+  var imagini = verificareImagini();
+  if(imagini.length > 6 && imagini.length < 12){
+    while(imagini.length != 6){
+      imagini.pop();
+    }
+  }
   const ip = req.clientIp;
   console.log('Request pentru pagina principala. Returnez index.ejs');
-  res.render('pagini/index.ejs', {ip: ip});
+  res.render('pagini/index.ejs', {ip: ip, imagini:imagini, n: imagini.length});
 });
 
-router.get('/index', (req, res) => {
-  console.log('Request pentru pagina principala. Returnez index.ejs');
-  res.render('pagini/index.ejs');
-});
+// router.get('/index', (req, res) => {
+//   const ip = req.clientIp;
+//   console.log('Request pentru pagina principala. Returnez index.ejs');
+//   res.render('pagini/index.ejs', {ip: ip, imagini:verificareImagini()});
+// });
 
 router.get('/investigatii', (req, res) => {
   console.log('Request pentru pagina de investigatii. Returnez investigatii.ejs');
@@ -32,7 +99,7 @@ router.get('/investigatii', (req, res) => {
 
 router.get('/departamente', (req, res) => {
   console.log('Request pentru pagina de departamente. Returnez departamente.ejs');
-  res.render('pagini/departamente.ejs');
+  res.render('pagini/departamente.ejs', {imagini:alegImagini()});
 })
 
 module.exports = router;
